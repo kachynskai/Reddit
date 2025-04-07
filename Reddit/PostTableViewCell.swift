1ff482b4c552c5aefdc5d8dd45c965bc112c5ea4
kachynskai <105295714+kachynskai@users.noreply.github.com>
2025-04-07 22:49:24 +0300

    done practice 3

---
+++ Reddit/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  Reddit
+//
+//  Created by Iryna on 25.03.2025.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
